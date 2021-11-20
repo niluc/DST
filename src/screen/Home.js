6d4a3af4156dfff2c1d0ef8c22083e598776b923
thaikhuong62000 <thaikhuong62000@gmail.com>
2021-11-20 20:19:17 +0700

    Finish Home Screen

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,42 +1,64 @@
 import React, {useEffect} from 'react';
-import {
-  StyleSheet,
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-  View,
-  Button,
-} from 'react-native';
+import {StyleSheet, SafeAreaView} from 'react-native';
 import BigList from '../BigList';
 import Entries from '../Entries';
 import SmallList from '../SmallList';
 import {ENTRIES_KEY, getData, clear} from '../Storage';
+import {isSameMonth} from 'date-fns';
 
 const Home = ({navigation}) => {
-  let biglist = [
-    {
-      id: 1,
-      title: 'Total Salary',
-      value: 150,
-    },
-    {
-      id: 2,
-      title: 'Expense',
-      value: 300,
-    },
-    {
-      id: 3,
-      title: 'Expense Month',
-      value: 300,
-    },
-  ];
   const [entries, setEntries] = React.useState([]);
-  const [bigList, setBigList] = React.useState(biglist);
+  const [bigList, setBigList] = React.useState([]);
   const [entriesChange, setEntriesChange] = React.useState(true);
+
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
       getData(ENTRIES_KEY).then(data => {
-        setEntries(JSON.parse(data).slice(0, 20));
+        let tempEntries = JSON.parse(data);
+        setEntries(tempEntries.slice(0, 20));
+
+        // Get total salary and expense
+        let salary = tempEntries
+          .filter(item => item.value > 0)
+          .reduce((x, y) => x + y.value, 0);
+        let expense = -tempEntries
+          .filter(item => item.value < 0)
+          .reduce((x, y) => x + y.value, 0);
+        // Get month salary and expense
+        let currentDay = new Date();
+        let monthEntries = tempEntries.filter(item => {
+          let parseDay = item.date.split('/').reverse();
+          parseDay[1] -= 1;
+          return isSameMonth(currentDay, new Date(...parseDay));
+        });
+        let month_salary = monthEntries
+          .filter(item => item.value > 0)
+          .reduce((x, y) => x + y.value, 0);
+        let month_expense = -monthEntries
+          .filter(item => item.value < 0)
+          .reduce((x, y) => x + y.value, 0);
+        setBigList([
+          {
+            id: 1,
+            title: 'Monthly Salary',
+            value: month_salary,
+          },
+          {
+            id: 2,
+            title: 'Monthly Expense',
+            value: month_expense,
+          },
+          {
+            id: 3,
+            title: 'Total Salary',
+            value: salary,
+          },
+          {
+            id: 4,
+            title: 'Expense',
+            value: expense,
+          },
+        ]);
         setEntriesChange(false);
         setEntriesChange(true);
       });
@@ -47,7 +69,7 @@ const Home = ({navigation}) => {
     <SafeAreaView style={styles.safe}>
       {/*
        */}
-      <BigList data={bigList} />
+      {entriesChange && <BigList data={bigList} />}
       <SmallList navigation={navigation} />
       {entriesChange && <Entries data={entries} navigation={navigation} />}
     </SafeAreaView>
